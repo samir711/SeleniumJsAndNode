e5daf125d8333db01ee262fe59e706a1a5e29792
samir711 <samirdoc1@gmail.com>
2018-05-16 22:23:23 -0400

    adding filew 16-May-2018

--- firefoxEmployeeLogin.js
+++ firefoxEmployeeLogin.js
@@ -29,12 +29,14 @@ var service = new firefox.ServiceBuilder(path).build();
 // firefox.setDefaultService(service);
 
 let driver = new WebDriver.Builder().forBrowser('firefox').setFirefoxOptions(firefoxOptions).build();
-driver.manage().window().maximize();
+//driver.manage().window().maximize();
 const newLocal =
  driver.get('http://desktop-llgeu3m:90/Account/Login').then(driver.findElement(By.name('UserName')).sendKeys('admin'));
  driver.findElement(By.name('Password')).sendKeys('password').then(driver.findElement(By.css('input[type="submit"]')).click());
- driver.sleep(5000);
- let element = driver.findElement(By.xpath('.//*[text() = "Log off"]'));
- driver.wait(WebDriver.until.elementLocated(By.xpath('.//*[text() = "Log off"]'),30000));
+ driver.wait(WebDriver.until.elementLocated(By.xpath('.//*[contains(text(),"Log")]'),30000)); //driver.sleep(5000);
+ 
+
+ let element = driver.findElement(By.xpath('.//a[contains(text(),"Details")]'));
+ console.log('test value is ' + element.getAttribute('href'));
  //driver.wait(until.elementTextIs(,"Hello Admin!"),10000));
  
